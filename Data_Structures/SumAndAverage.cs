8882bc90fbb56b29f3f7809d63585186aa3663b0
Yoanna Atanasova <bitofgeek@gmail.com>
2016-02-24 21:50:10 +0200

    Update SumAndAverage.cs

--- Data_Structures/SumAndAverage.cs
+++ Data_Structures/SumAndAverage.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lists{
+namespace SumAndAverage{
     class SumAndAverage{
 
         static void action(List<int> list) {
