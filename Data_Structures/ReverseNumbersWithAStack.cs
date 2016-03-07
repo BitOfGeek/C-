131793d133b1e8142e3e06005eec34ad52687630
Yoanna Atanasova <bitofgeek@gmail.com>
2016-03-07 21:52:30 +0200

    Added files via upload

---
+++ Data_Structures/ReverseNumbersWithAStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumbersWithAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line of numbers: ");
+            var stackOfNumbers = new Stack<int>();
+            var line = Console.ReadLine();
+            var numbers = line.Split(' ');
+            foreach (var number in numbers)
+            {
+                int num;
+                if (Int32.TryParse(number, out num))
+                {
+                    stackOfNumbers.Push(num);
+                }
+            }
+            while (stackOfNumbers.Count >0)
+            {
+                Console.WriteLine(stackOfNumbers.Pop());
+            }
+        }
+    }
+}
