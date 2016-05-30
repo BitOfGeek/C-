c12223c7b80ee376fb7097e05113ddc74afc1f2c
Yoanna Atanasova <atanasova.yoanna@gmail.com>
2016-05-30 09:49:15 +0300

    Kruskal and Dijkstra

---
+++ Algorithms/Kruskal_Diikstra/Dijkstra/DijkstraWithoutQueue.cs
@@ -0,0 +1,45 @@
+﻿namespace Dijkstra
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class DijkstraWithoutQueue
+    {
+        public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
+        {
+            int n = graph.GetLength(0);
+            int[] distance = new int[n];
+            var used = new bool[n];
+            int?[] previous = new int?[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                distance[i] = int.MaxValue;
+            }
+
+            distance[sourceNode] = 0;
+
+            while (true)
+            {
+                int midDistance = int.MaxValue;
+                int minNode = 0;
+
+                for (int node = 0; node < n; node++)
+                {
+                    if (!used[node] && distance[node] < midDistance)
+                    {
+                        midDistance = distance[node];
+                        minNode = node;
+                    }
+
+                    if (midDistance == int.MaxValue)
+                    {
+                        break;
+                    }
+
+                    used[minNode] = true;
+                }
+            }
+        }
+    }
+}
